70c6e67c116f0dc77586b1df8d1704880d27dfaa
Casciper <pechenkov39@gmail.com>
2021-10-01 14:21:50 +0300

    Update main.js
    used function

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ function randomInteger(min, max) {
   let rand = min + Math.random() * (max + 1 - min);
   return Math.floor(rand);
 }
-
+randomInteger(1,100);
 console.log(randomInteger(1,100));
 
 
@@ -11,5 +11,5 @@ function randomFloat(min, max, fixed) {
   let rand = Number((min + Math.random() * (max + 1 - min)).toFixed(fixed));
   return rand;
 }
-
+randomFloat(1,100,2);
 console.log(randomFloat(1,100,2));
