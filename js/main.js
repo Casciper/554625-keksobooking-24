bc88147ec837b6b76d73db6e1f850150f49af3c3
Casciper <pechenkov39@gmail.com>
2021-10-02 23:31:01 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,8 +3,10 @@ const randomInteger = (min, max) => {
   const rand = min + Math.random() * (max + 1 - min);
   if(max < 0 || min < 0){
     console.log('В диапозоне доступны только положительные числа...');
+    return
   }else if(min > max){
     console.log('Минимальное значение не может быть больше максимального...');
+    return
   }else{
   return Math.floor(rand);
   }
@@ -17,8 +19,10 @@ const randomFloat = (min, max, fixed) => {
   const rand = Number((min + Math.random() * (max + 1 - min)).toFixed(fixed));
   if((max < 0) || (min < 0)){
     console.log('В диапозоне доступны только положительные числа...');
+    return
   }else if(min > max){
     console.log('Минимальное значение не может быть больше максимального...');
+    return
   }else{
   return rand;
   }
